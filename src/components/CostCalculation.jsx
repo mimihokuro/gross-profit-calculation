d2669f42e1c85f3aeca7dabcbf0841fb3561f3c7
mimihokuro <keetasan7@gmail.com>
2024-10-05 21:45:35 +0900

    🎨 原価計算部分をChakra UIに置き換え

--- src/components/CostCalculation.jsx
+++ src/components/CostCalculation.jsx
@@ -1,7 +1,5 @@
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Button, Input, Stack, Text } from "@chakra-ui/react";
 import { useState } from "react";
-import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
-import { ArrowForwardIos } from "@mui/icons-material";
 
 const CostCalculation = () => {
   const [sellingPrice, setSellingPrice] = useState("");
@@ -42,56 +40,52 @@ const CostCalculation = () => {
       <Stack
         alignItems="center"
         justifyContent="space-between"
-        sx={{
-          flexDirection: { xs: "column", sm: "row" },
-          gap: { xs: "0", sm: "16px" },
-        }}
+        direction={{ base: "column", sm: "row" }}
+        gap={4}
       >
         <Stack spacing={4} backgroundColor="#f5f5f5" borderRadius={2} p={2}>
           {GROSS_MARGIN_RATIO_ITEMS.map((item) => {
             return (
-              <Stack
-                spacing={1}
-                key={item.label}
-                direction="row"
-                alignItems="center"
-                sx={{
-                  maxWidth: { sm: "160px" },
-                }}
-              >
-                <TextField
-                  id="outlined-basic"
-                  label={item.label}
-                  variant="outlined"
-                  value={item.type}
-                  onChange={(e) => item.func(e.target.value)}
-                />
-                <Typography variant="subtitle1">{item.unit}</Typography>
+              <Stack key={item.label}>
+                <Text fontSize="14px">{item.label}</Text>
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  maxWidth={{ sm: "160px" }}
+                >
+                  <Input
+                    value={item.type}
+                    label={item.label}
+                    onChange={(e) => item.func(e.target.value)}
+                    borderColor="#aaaaaa"
+                    focusBorderColor="teal.400"
+                  />
+                  <Text variant="subtitle1">{item.unit}</Text>
+                </Stack>
               </Stack>
             );
           })}
         </Stack>
-        <Typography
+        <Text
           fontWeight="bold"
-          fontSize={32}
-          sx={{
-            rotate: { xs: "90deg", sm: "0deg" },
-          }}
+          fontSize={24}
+          transform={{ base: "rotate(90deg)", sm: "rotate(0deg)" }}
         >
-          <ArrowForwardIos sx={{ fontSize: 40 }} />
-        </Typography>
+          ＞
+        </Text>
         <Button
-          variant="contained"
           fontWeight="bold"
+          variant="filled"
+          backgroundColor="teal.400"
+          color="#ffffff"
           sx={{ fontSize: "20px" }}
           onClick={calculationCost}
-          endIcon={<RocketLaunchIcon />}
         >
           計算実行
         </Button>
         <Stack
           flexGrow={1}
-          borderRadius={2}
+          borderRadius={8}
           gap={1}
           py={4}
           px={2}
@@ -105,16 +99,16 @@ const CostCalculation = () => {
           alignItems="center"
         >
           <Stack direction="row" alignItems="center" flexWrap="wrap">
-            <Typography variant="subtitle1" fontSize={24} lineHeight="1">
+            <Text variant="subtitle1" fontSize={24} lineHeight="1">
               原価：
-            </Typography>
+            </Text>
             <Stack direction="row" alignItems="center">
-              <Typography variant="subtitle1" fontSize={36} lineHeight="1">
+              <Text variant="subtitle1" fontSize={36} lineHeight="1">
                 {cost}
-              </Typography>
-              <Typography variant="subtitle1" fontSize={24} lineHeight="1">
+              </Text>
+              <Text variant="subtitle1" fontSize={24} lineHeight="1">
                 円
-              </Typography>
+              </Text>
             </Stack>
           </Stack>
         </Stack>
